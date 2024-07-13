2d0807d079fbe8a0bafc1f83a7d1b9a514e8cb12
xszqxszq <943551369@qq.com>
2024-07-13 22:24:44 +0800

    Use redis to share login status

--- parent/admin/src/main/java/com/example/AdminApplication.java
+++ parent/admin/src/main/java/com/example/AdminApplication.java
@@ -3,9 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60 * 60)
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
--- parent/user/src/main/java/com/example/UserApplication.java
+++ parent/user/src/main/java/com/example/UserApplication.java
@@ -3,9 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60 * 60)
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
